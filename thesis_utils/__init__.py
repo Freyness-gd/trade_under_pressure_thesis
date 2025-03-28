3506523f375ea9617c2ccec2aa0b892ea51f1838
freyness <gabriel@dinu.tech>
2025-03-29 00:06:12 +0100

    :package: Add thesis_utils package

---
+++ thesis_utils/__init__.py
@@ -0,0 +1,1 @@
+from .clean.clean_gdp import *
---
+++ thesis_utils/clean/clean_gdp.py
@@ -0,0 +1,58 @@
+from pandas import DataFrame
+
+import Constants
+
+
+def clean_gdp(
+    data: DataFrame,
+    start_year=Constants.START_YEAR,
+    end_year=Constants.END_YEAR,
+    missing_threshold=Constants.MISSING_THRESHOLD,
+    col_first=False,
+):
+    if start_year < Constants.START_YEAR or end_year > Constants.END_YEAR:
+        raise IndexError("Start or End Year out of bounds")
+
+    data = data.drop(["Series Name", "Series Code", "Country Name"], axis=1)
+    data = data.rename(columns=lambda x: x if not x.endswith("]") else x.split(" ")[0])
+    data = data.rename(columns={"Country Code": "ISO3"})
+    data = data[~data["ISO3"].isna()]
+
+    # Include only subset of years
+    data = data[["ISO3"] + [str(year) for year in range(start_year, end_year + 1)]]
+
+    data = remove_rows_with_missing(data, missing_threshold, col_first)
+
+    return data
+
+
+def remove_rows_with_missing(
+    data: DataFrame, missing_threshold=Constants.MISSING_THRESHOLD, col_first=False
+):
+    # TODO: Log with DEBUG
+    # print("Length before filter: ", data.shape)
+    # for row in data.itertuples(name="Country"):
+    #     print(
+    #         data.loc[row.Index]["ISO3"],
+    #         ": ",
+    #         (data.loc[row.Index].isna().sum() / len(data.loc[row.Index])),
+    #         "\n",
+    #     )
+    #     print("Len ", len(data.loc[row.Index]), "\n")
+    # print("Len: ", len(data.columns))
+
+    if col_first:
+        data = data.loc[:, data.isnull().sum() / len(data) <= missing_threshold]
+        data = data[
+            (data.isnull().sum(axis=1) / len(data.columns)) <= missing_threshold
+        ]
+    else:
+        data = data[
+            (data.isnull().sum(axis=1) / len(data.columns)) <= missing_threshold
+        ]
+        data = data.loc[:, data.isnull().sum() / len(data) <= missing_threshold]
+
+    # TODO: Log with Debug
+    # print("Length after filter: ", data.shape)
+
+    return data

e8d5c2691729e35d5cef26f9c488fa4666b6d4f0
freyness <gabriel@dinu.tech>
2025-04-24 09:18:23 +0200

    :loud_sound: Update logger setup

--- thesis_utils/__init__.py
+++ thesis_utils/__init__.py
@@ -4,3 +4,4 @@ from .clean.clean_gdp import *
 from .clean.clean_geopa import *
 from .clean.clean_gsdb import *
 from .constants import Constants
+from .set_up_logger import set_up_logger
---
+++ thesis_utils/set_up_logger.py
@@ -0,0 +1,13 @@
+from typing import TextIO
+
+from loguru import logger as log
+
+
+def set_up_logger(stream: TextIO):
+  log.remove()
+  log.add(
+    stream,
+    colorize=True,
+    format="<w>{time:DD/MM HH:mm}</w> - <level>{level}</level>: {message}\n",
+  )
+  return log

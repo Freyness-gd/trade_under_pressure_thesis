b30161c374792d1c6b2bdeb1ecb1b47b3d7b0a51
freyness <gabriel@dinu.tech>
2025-04-24 09:18:30 +0200

    :loud_sound: Update logger setup

--- thesis_utils/set_up_logger.py
+++ thesis_utils/set_up_logger.py
@@ -4,10 +4,10 @@ from loguru import logger as log
 
 
 def set_up_logger(stream: TextIO):
-  log.remove()
-  log.add(
-    stream,
-    colorize=True,
-    format="<w>{time:DD/MM HH:mm}</w> - <level>{level}</level>: {message}\n",
-  )
-  return log
+    log.remove()
+    log.add(
+        stream,
+        colorize=True,
+        format="<w>{time:DD/MM HH:mm}</w> - <level>{level}</level>: {message}\n",
+    )
+    return log
